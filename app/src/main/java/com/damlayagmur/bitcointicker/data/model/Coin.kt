b25d29724a247fdc6c762cfa7f4df5bb9d867130
damlayagmr <damlayagmur976@gmail.com>
2022-08-22 17:38:02 +0300

    Add  Coin models

---
+++ app/src/main/java/com/damlayagmur/bitcointicker/data/model/Coin.kt
@@ -0,0 +1,3 @@
+package com.damlayagmur.bitcointicker.data.model
+
+class Coin : ArrayList<CoinItem>()
---
+++ app/src/main/java/com/damlayagmur/bitcointicker/data/model/CoinItem.kt
@@ -0,0 +1,7 @@
+package com.damlayagmur.bitcointicker.data.model
+
+data class CoinItem(
+    val id: String,
+    val name: String,
+    val symbol: String
+)
