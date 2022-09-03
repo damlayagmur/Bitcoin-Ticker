8821e95092feda85e7b4e65db4b9fd9b23694f65
damlayagmr <damlayagmur976@gmail.com>
2022-09-03 17:04:36 +0300

    Add Fast Adapter for CoinList recyclerview

---
+++ app/src/main/java/com/damlayagmur/bitcointicker/adapter/CoinAdapter.kt
@@ -0,0 +1,38 @@
+package com.damlayagmur.bitcointicker.adapter
+
+import android.view.View
+import android.widget.TextView
+import com.damlayagmur.bitcointicker.R
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
+
+class CoinAdapter : AbstractItem<CoinAdapter.ViewHolder>() {
+
+    var name: String? = null
+    var desc: String? = null
+
+    override val layoutRes: Int
+        get() = R.layout.coin_list_item
+    override val type: Int
+        get() = com.mikepenz.fastadapter.R.id.fastadapter_item_adapter
+
+    override fun getViewHolder(v: View): ViewHolder {
+        return ViewHolder(v)
+    }
+
+    class ViewHolder(view: View) : FastAdapter.ViewHolder<CoinAdapter>(view) {
+
+        var tvName: TextView = view.findViewById(R.id.tvName)
+        var tvDesc: TextView = view.findViewById(R.id.tvDesc)
+
+        override fun bindView(item: CoinAdapter, payloads: List<Any>) {
+            tvName.text = item.name
+            tvDesc.text = item.desc
+        }
+
+        override fun unbindView(item: CoinAdapter) {
+            tvName.text = null
+            tvDesc.text = null
+        }
+    }
+}
